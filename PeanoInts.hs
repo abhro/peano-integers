8214a1767d6a3629e5946075853c6368338b1ab0
abhro <abhro@users.noreply.github.com>
2021-07-19 09:33:28 +0800

    Add function to canonicalize Integers

--- PeanoInts.hs
+++ PeanoInts.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 -- Implementation of all Integers, not just natural numbers via the Peano axioms
--- Names in this file may clash with the defaults/bultins in Haskell libraries,
+-- Names in this file may clash with the defaults/built-ins in Haskell libraries,
 -- but this file is for taking notes and doing homework and not use as a library
 -- (for this reason no traits/typeclasses are even mentioned, to keep focus on
 -- data and algorithm rather than grouping of said algorithms)
@@ -16,3 +16,13 @@ import Prelude (Show, undefined)
 -- See https://en.wikipedia.org/wiki/Recursive_definition (also called inductive
 -- definition) for more details.
 data Integer = Zero | Pred Integer | Succ Integer deriving Show
+
+
+-- simplify chains of Pred and Succ which should never be chained together in
+-- canonical form
+simplify :: Integer -> Integer
+simplify (Pred (Succ n)) = reduce n
+simplify (Succ (Pred n)) = reduce n
+simplify (Succ n)        = Succ (reduce n)
+simplify (Pred n)        = Pred (reduce n)
+simplify n               = n
